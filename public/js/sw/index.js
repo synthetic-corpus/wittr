120ddb8e9b2e384fb7eb4c75f6379ec90b6d2daa
Joel Gonzaga <joel.gonzaga@usaepay.com>
2018-01-12 16:53:48 -0800

    completed the headers as expected.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  // console.log(event.request);
+  event.respondWith(
+      new Response("<p class='a-winner-is-me'>Is this HTML?</p>",
+        {headers: {'Content-Type':'text/html'}})
+  );
 });
