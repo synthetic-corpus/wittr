26b06070f4dc791c3c4d5aecb070b0ff05df934a
Joel Gonzaga <joel.gonzaga@usaepay.com>
2018-01-13 16:29:36 -0800

    passed another hurdle.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,12 @@
+
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  let suffix = event.request.url.slice(-4);
+  if (suffix === '.jpg'){
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
+  
 });
